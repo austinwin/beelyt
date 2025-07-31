7f2568a00d1d45cabe302dd34492031942587ac5
austinwin <145726754+austinwin@users.noreply.github.com>
2025-07-31 03:21:47 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,14 +2,13 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('habit-tracker-cache').then(cache => {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/css/styles.css',
-        '/js/utils.js',
-        '/js/habits.js',
-        '/js/app.js',
-        '/icons/icon-192x192.png',
-        '/icons/icon-512x512.png'
+        'index.html',
+        'css/styles.css',
+        'js/utils.js',
+        'js/habits.js',
+        'js/app.js',
+        'icons/icon-192x192.png',
+        'icons/icon-512x512.png'
       ]);
     })
   );
