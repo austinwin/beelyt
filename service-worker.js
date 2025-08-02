c44c53d85a844500a5448773bb9741dabf1ec9b1
austinwin <145726754+austinwin@users.noreply.github.com>
2025-08-02 07:18:36 -0500

    Changed to stacked layout

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'habit-tracker-v1.1';  // Add versioning
+const CACHE_NAME = 'habit-tracker-v1.3';  // Add versioning
 // NOTE: Increment CACHE_NAME when deploying new versions to force cache update.
 //8-1-2025: release v1.1 - force to auto-update
 const FILES_TO_CACHE = [
@@ -56,4 +56,5 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+
 
